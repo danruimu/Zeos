6e1bc643375fcae3b71b5bcace74d04b597cb03c
darak1313 <dani.ruiz.89@gmail.com>
2012-11-24 11:08:48 +0100

    Copiamos direcciones de memoria

--- sys.c
+++ sys.c
@@ -142,9 +142,9 @@ int sys_clone(void (*function)(void), void *stack) {
             : "=g"(ebp)); //obtenim el punter al ebp del actual
     int desp = ((unsigned long*) ebp - &actual->stack[0]); //calculem quantes celes de mem hi ha entre l'inici i el esp
     nou->stack[desp] = ebp;
-    nou->stack[desp + 1] = (unsigned long) ret_from_clone; //posem una posicio per la pila amunt per on retornarà el fill
-    nou->stack[desp - 2] = (unsigned long) stack; //posem dos posicions per la pila amunt el ebp del pare
-    nou->stack[desp - 1] = (unsigned long) function;
+    nou->stack[desp + 1] = (unsigned long) &ret_from_clone; //posem una posicio per la pila amunt per on retornarà el fill
+    nou->stack[desp - 2] = (unsigned long) &stack; //posem dos posicions per la pila amunt el ebp del pare
+    nou->stack[desp - 1] = (unsigned long) &function;
     nou->task.kernel_esp = (unsigned int) &nou->stack[desp]; //diem que el kernel_esp del fill sigui la posició del ebp del pare
     list_add_tail(&nou->task.entry, &readyQueue);
     return 0;
