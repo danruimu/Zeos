379155e9cb9f2ee71417ea3c0ef2077a29afd4e4
darak1313 <dani.ruiz.89@gmail.com>
2012-11-29 15:42:22 +0100

    Diferentes pruebas

--- user.c
+++ user.c
@@ -2,21 +2,25 @@
 
 int pid;
 
-char buff[5];
+char *buff = "h";
 char stack[100];
 
 void pfunct(void) {
 	int res;
+	buff = "c";
+	pid = getpid();
+	atoi(buff, &pid);
 	printz(buff);
-	res = read(0, buff, 5);
-	if(res > 0) printz(buff);
-	else perror("");
+	//res = read(0, buff, 5);
+	//printz(buff);
 	exit();
 }
 
 int __attribute__((__section__(".text.main")))
 main(void) {
     clone(pfunct, stack);
+    buff = "l";
+    printz(buff);
     while (1);
     return 0;
 }
