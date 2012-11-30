92774aaf9361720bda37e53fd1b89466dd62064e
darak1313 <dani.ruiz.89@gmail.com>
2012-11-30 08:12:18 +0100

    Arreglamos mierda que habia de ficheros raros eliminados y polladas asi

--- sys_call_table.s
+++ sys_call_table.s
@@ -4,7 +4,7 @@
 # 1 "sys_call_table.S"
 # 1 "include/asm.h" 1
 # 2 "sys_call_table.S" 2
-# 1  "include/segment.h" 1
+# 1 "include/segment.h" 1
 # 3 "sys_call_table.S" 2
 
 .globl sys_call_table; .type sys_call_table, @function; .align 0; sys_call_table:
