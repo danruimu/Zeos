ce4c16d43220b8f78ba02deeddfb408c3fd99ccc
dani <dani@V3-551G>
2012-11-30 15:47:27 +0100

    Configuración del user.c para probar el read y el clone.

--- user.c
+++ user.c
@@ -1,27 +1,19 @@
 #include <libc.h>
 
-int *pid;
-
 char buff[100];
 char stack[100];
 
 void pfunct(void) {
-        //buff[0] = 'h';
 	printz("pfunct\n");
-	//read(0, buff, 5);
-	//printz("no se bloquea\n");
-	//printz(buff);
-	itoa(*pid,buff);
+	read(0, buff, 5);
+	printz("me he desbloqueado\n");
 	printz(buff);
 	exit();
 }
 
 int __attribute__((__section__(".text.main")))
 main(void) {
-	int a = 3;
-	pid = &a;
     clone(pfunct, stack);
-    
     printz("Main\n");
     while (1);
     return 0;
