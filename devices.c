d26008ab6357df93a14314d4ddf2df7fce07962f
dani <dani@V3-551G>
2012-11-30 15:45:04 +0100

    Posible buena implementación del readChar

--- devices.c
+++ devices.c
@@ -19,7 +19,7 @@ int llistaTeclatBuida() {
 void readChar() {
     if(finBuffer<TAM_BUFF && iniBuffer == 0) {  //caso buffer inicial o completo desde 0 a TAM_BUFF-1
         read_buff[finBuffer++] = llegirImprimirTecla();
-    } else if(finBuffer < iniBuffer) {          //caso ya tenemos el buffer circular
+    } else if(finBuffer < iniBuffer-1) {          //caso ya tenemos el buffer circular
         read_buff[finBuffer++] = llegirImprimirTecla();
     } else if(!finBuffer && !iniBuffer) {       //primera tecla pulsada en general
         read_buff[finBuffer++] = llegirImprimirTecla();
@@ -27,19 +27,39 @@ void readChar() {
     //si finBuffer==TAM_BUFF y iniBuffer==0 o finBuffer==iniBuffer!=0 tenemos el buffer
     //completo y no tenemos que leer
     if(!list_empty(&blocked)) {
-	struct readStruct *lectorActual;
-	lectorActual = list_head_to_lectura(list_first(blocked));
-	if(lectorActual->blocsLlegits * TAM_BUFF + finBuffer == lectorActual->tamany) {
-		copy_to_user(&read_buff[0], &lectorActual->buffer[lectorActual->blocsLlegits * TAM_BUFF], finBuffer);
-		list_del(&lectorActual->PCB->entry);
-		encuaReady(lectorActual->PCB);
-		return;
-	}
-	if(finBuffer == TAM_BUFF) {
-		copy_to_user(&read_buff[0], &lectorActual->buffer[lectorActual->blocsLlegits * TAM_BUFF], TAM_BUFF);
-		finBuffer = 0;
-		lectorActual->blocsLlegits++;
-	}
+        struct readStruct *lectorActual;
+        lectorActual = list_head_to_lectura(list_first(blocked));
+        if(finBuffer > iniBuffer) {
+            if(lectorActual->tamany <= TAM_BUFF) {
+                if(lectorActual->tamany == (finBuffer - iniBuffer + 1) ) {
+                    copy_to_user(&read_buff[iniBuffer], lectorActual->buffer, finBuffer-iniBuffer + 1);
+                    iniBuffer = finBuffer = 0;
+                    list_del(&lectorActual->PCB->entry);
+                    encuaReady(lectorActual->PCB);
+                }
+            } else {
+                if(TAM_BUFF == (finBuffer - iniBuffer + 1) ) {
+                    copy_to_user(&read_buff[iniBuffer], lectorActual->buffer, finBuffer-iniBuffer + 1);
+                    iniBuffer = finBuffer = 0;
+                    lectorActual->blocsLlegits++;
+                    lectorActual->tamany -= TAM_BUFF;
+                }
+            }
+        } else if (iniBuffer > finBuffer) {
+            if(lectorActual->tamany <= ((TAM_BUFF - iniBuffer +1) + finBuffer + 1) ) {
+                copy_to_user(&read_buff[iniBuffer], lectorActual->buffer, TAM_BUFF - iniBuffer + 1);
+                copy_to_user(&read_buff[0], &lectorActual->buffer[TAM_BUFF - iniBuffer + 2], finBuffer+1);
+                iniBuffer = finBuffer = 0;
+                list_del(&lectorActual->PCB->entry);
+                encuaReady(lectorActual->PCB);
+            } else {
+                copy_to_user(&read_buff[iniBuffer], lectorActual->buffer, TAM_BUFF - iniBuffer + 1);
+                copy_to_user(&read_buff[0], &lectorActual->buffer[TAM_BUFF - iniBuffer + 2], finBuffer+1);
+                iniBuffer = finBuffer = 0;
+                lectorActual->blocsLlegits++;
+                lectorActual->tamany -= TAM_BUFF;
+            }
+        }
     }
 }
 
