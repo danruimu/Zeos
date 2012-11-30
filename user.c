d6f089e7759e005b24fa02f48c0548ec80b813a5
dani <dani@V3-551G>
2012-11-30 09:28:24 +0100

    Probamos getpid y fork y clone

--- user.c
+++ user.c
@@ -17,7 +17,19 @@ void pfunct(void) {
 
 int __attribute__((__section__(".text.main")))
 main(void) {
-    clone(pfunct, stack);
+    //clone(pfunct, stack);
+    pid = fork();
+    if(pid) {   //padre
+        itoa(pid, buff);
+        printz("pid hijo =  ");
+        printz(buff);
+        printz("pid padre = ");
+        pid = getpid();
+        itoa(pid, buff);
+        printz(buff);
+    } else {    //hijo
+        clone(pfunct, stack);
+    }
     printz("Main\n");
     while (1);
     return 0;
