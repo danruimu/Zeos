6b9ecb1c5c6a9d4c6c348a7662ef897604e5a9c6
dani <dani@V3-551G>
2012-11-30 08:08:38 +0100

    Revert "Diferentes pruebas"
    This reverts commit 379155e9cb9f2ee71417ea3c0ef2077a29afd4e4.

--- user.c
+++ user.c
@@ -2,25 +2,21 @@
 
 int pid;
 
-char *buff = "h";
+char buff[5];
 char stack[100];
 
 void pfunct(void) {
 	int res;
-	buff = "c";
-	pid = getpid();
-	atoi(buff, &pid);
 	printz(buff);
-	//res = read(0, buff, 5);
-	//printz(buff);
+	res = read(0, buff, 5);
+	if(res > 0) printz(buff);
+	else perror("");
 	exit();
 }
 
 int __attribute__((__section__(".text.main")))
 main(void) {
     clone(pfunct, stack);
-    buff = "l";
-    printz(buff);
     while (1);
     return 0;
 }
