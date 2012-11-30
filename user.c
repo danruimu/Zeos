de57a7cacac21151e93538418ea282601f1e4a16
darak1313 <dani.ruiz.89@gmail.com>
2012-11-30 08:16:46 +0100

    Añadimos comprobación para demostrar que no se puede obtener el PID de un proceso

--- user.c
+++ user.c
@@ -1,6 +1,6 @@
 #include <libc.h>
 
-int pid;
+int pid = -20;
 
 char buff[5];
 char stack[100];
@@ -16,7 +16,11 @@ void pfunct(void) {
 
 int __attribute__((__section__(".text.main")))
 main(void) {
-    clone(pfunct, stack);
+    //clone(pfunct, stack);
+    char *buffer = "p";
+    pid = getpid();
+    atoi(buffer, &pid);
+    printz(buff);
     while (1);
     return 0;
 }
